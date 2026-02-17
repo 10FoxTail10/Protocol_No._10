b2ae9fd8629c8485a0192bda09e6a2dbda2f8be3
FoxTail <10foxtail10@gmail.com>
2026-02-17 04:37:41 +0300

    Добавление звуков и изменение скрипта

---
+++ Protocol; No. 10/Assets/Scripts/GlobalSet.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class GlobalSet : MonoBehaviour
+{
+    //#region Global_Setting
+
+    //#endregion
+
+    //#region Player_Setting
+    //public TMP_Text tip;   // UI текст, куда выводится подсказка
+    //private bool isInRange = false; // Флаг близости к объекту
+
+    //#endregion
+
+    //#region Game_Setting
+    //private float globalVolume = 1f; // Базовое значение звука в игре (Значение от 0 (без звука) до 1 (полная громкость))
+    //private float musicVolume = 1f; // Базовое значение музыки в игре (Значение от 0 (без звука) до 1 (полная громкость))
+    //private float effectVolume = 1f; // Базовое значение музыки в игре (Значение от 0 (без звука) до 1 (полная громкость))
+    //void Start()
+    //{
+    //    tip.text = ""; // Базовое значение подсказки для игрока в начале игры
+    //    audioTV.volume = startVolume; // Изменение на базовое значение звука
+    //}
+
+    //#endregion
+
+    //#region HotKey_Setting
+
+    //#endregion
+}
--- Protocol; No. 10/Assets/Scripts/Press_E.cs
+++ Protocol; No. 10/Assets/Scripts/Press_E.cs
@@ -7,49 +7,53 @@ using TMPro;
 
 public class Press_E : MonoBehaviour
 {
-    public TMP_Text tip;   // UI текст, куда выводится подсказка
-    public AudioSource audioTV; // Аудио плеер объекта
-    public MeshRenderer videoTV; // Mesh объект
+    public TMP_Text tip;   // UI текст, куда выводится подсказка        #Перевести в один файл#
+    public AudioSource audioTV; // Аудио плеер телевизора
+    public MeshRenderer videoTV; // Mesh объект телевизора
+    public AudioClip soundEffect; // Звуковой эффект подбора вещей
+    public GameObject Watch;
 
+    private bool isInRangeTV = false; // Флаг близости к TV
     private bool isInRange = false; // Флаг близости к объекту
-    private bool screenIsActive = true; // Флаг активности экрана
-    private float startVolume = 1f; // Базовое значение звука в игре (Значение от 0 (без звука) до 1 (полная громкость))
+    private bool screenIsActive = true; // Флаг активности экрана телевизора        #Перевести в один файл#
+    private float startVolume = 1f; // Базовое значение звука в игре (Значение от 0 (без звука) до 1 (полная громкость))        #Перевести в один файл#
 
     void Start()
     {
         tip.text = ""; // Базовое значение подсказки для игрока в начале игры
         audioTV.volume = startVolume; // Изменение на базовое значение звука
-    }
+    }       //#Перевести в один файл#
 
     void Update()
     {
         ChangeActiveTV(); //Смена активности TV
+        CollectObject(); // Сбор предметов 
     }
 
-    #region TV
+    #region Change_Active_TV
     private void ChangeActiveTV()
     {
-        if (isInRange && screenIsActive && hasTV)
+        if (isInRangeTV && screenIsActive)
         {
-            tip.text = "Нажмите 'E', чтобы выключить TV";
+            tip.text = "Нажмите 'E', чтобы выключить";
             if (Input.GetKeyDown(KeyCode.E) && screenIsActive)
             {
                 TurnOffTV();
                 screenIsActive = false;
             }
         }
-        else if (isInRange && hasTV)
+        else if (isInRangeTV)
         {
-            tip.text = "Нажмите 'E', чтобы включить TV";
+            tip.text = "Нажмите 'E', чтобы включить";
             if (Input.GetKeyDown(KeyCode.E))
             {
                 TurnOnTV();
                 screenIsActive = true;
             }
         }
-        else
+        else if (!isInRangeTV && !isInRange)
         {
-            tip.text = "";
+            tip.text = ""; // Пустое собщение, чтобы не мешалось на экране)
         }
     }
 
@@ -66,10 +70,34 @@ public class Press_E : MonoBehaviour
     }
     #endregion
 
+    #region Collect
+    private void CollectObject()
+    {
+        if (isInRange && Watch != null)
+        {
+            tip.text = "Нажмите 'E'";
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CollectWatch();
+            }
+        }
+    }
+
+    private void CollectWatch()
+    {
+        Destroy(Watch);
+        AudioSource.PlayClipAtPoint(soundEffect, transform.position);
+    }
+    #endregion
+
     #region TriggerTag
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Screen"))
+        {
+            isInRangeTV = true;
+        }
+        if (other.CompareTag("Watch"))
         {
             isInRange = true;
         }
@@ -78,6 +106,10 @@ public class Press_E : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Screen"))
+        {
+            isInRangeTV = false;
+        }
+        if (other.CompareTag("Watch"))
         {
             isInRange = false;
         }
