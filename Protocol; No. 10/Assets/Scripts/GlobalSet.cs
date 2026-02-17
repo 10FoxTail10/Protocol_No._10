772c96d461958183b73cf5947b0bdd4d9e6bec1c
FoxTail <10foxtail10@gmail.com>
2026-02-18 01:10:13 +0300

    Небольшие изменения

--- Protocol; No. 10/Assets/Scripts/GlobalSet.cs
+++ Protocol; No. 10/Assets/Scripts/GlobalSet.cs
@@ -3,35 +3,38 @@ using TMPro;
 
 public class GlobalSet : MonoBehaviour
 {
+    #region Game_Setting
+    [Header("Game Setting")]
+    [SerializeField] public float globalVolume = 1f; // Базовое значение звука в игре (Значение от 0 (без звука) до 1 (полная громкость))
+    [SerializeField] public float musicVolume = 1f;
+    [SerializeField] public float effectVolume = 1f;
+    [SerializeField]
+
+    #endregion
+
+    #region Player_Setting
+    [Header("Player Setting")]
+    [SerializeField] public TMP_Text tip;   // UI текст, куда выводится подсказка
+    [SerializeField] public bool isInRange = false; // Флаг близости к объекту
+    [SerializeField]
+    [SerializeField]
+
+    #endregion
+
     public int assss = 15;
 
     void Update()
     {
         Debug.Log(assss);
     }
-    //#region Global_Setting
-
-    //#endregion
-
-    //#region Player_Setting
-    //public TMP_Text tip;   // UI текст, куда выводится подсказка
-    //private bool isInRange = false; // Флаг близости к объекту
-
-    //#endregion
-
-    //#region Game_Setting
-    //private float globalVolume = 1f; // Базовое значение звука в игре (Значение от 0 (без звука) до 1 (полная громкость))
-    //private float musicVolume = 1f; // Базовое значение музыки в игре (Значение от 0 (без звука) до 1 (полная громкость))
-    //private float effectVolume = 1f; // Базовое значение музыки в игре (Значение от 0 (без звука) до 1 (полная громкость))
-    //void Start()
-    //{
-    //    tip.text = ""; // Базовое значение подсказки для игрока в начале игры
-    //    audioTV.volume = startVolume; // Изменение на базовое значение звука
-    //}
-
-    //#endregion
 
     //#region HotKey_Setting
 
     //#endregion
+
+    void Start()
+    {
+        tip.text = ""; // Базовое значение подсказки для игрока в начале игры
+        audioTV.volume = startVolume; // Изменение на базовое значение звука
+    }
 }
